d7f19b393395bbed8ebaa188786562cc5ad17adf
Bernd Schoolmann <mail@quexten.com>
2023-01-06 12:21:05 +0100

    Add 2^15 as a minimum scrypt work factor

--- libs/common/src/enums/kdfType.ts
+++ libs/common/src/enums/kdfType.ts
@@ -7,4 +7,13 @@ export const DEFAULT_KDF_TYPE = KdfType.PBKDF2_SHA256;
 export const DEFAULT_KDF_ITERATIONS = 100000;
 export const SEND_KDF_ITERATIONS = 100000;
 export const DEFAULT_SCRYPT_WORK_FACTOR = 2 ** 16;
-export const SCRYPT_WORK_FACTORS = [2 ** 16, 2 ** 17, 2 ** 18, 2 ** 19, 2 ** 20, 2 ** 21, 2 ** 22];
+export const SCRYPT_WORK_FACTORS = [
+  2 ** 15,
+  2 ** 16,
+  2 ** 17,
+  2 ** 18,
+  2 ** 19,
+  2 ** 20,
+  2 ** 21,
+  2 ** 22,
+];
