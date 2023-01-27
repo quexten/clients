b77d863496946dd17ed85f58b89d89619a63d11d
Bernd Schoolmann <mail@quexten.com>
2023-01-27 14:13:56 +0100

    Implement biometrics for linux using PAM

--- apps/desktop/desktop_native/src/biometric/unix.rs
+++ apps/desktop/desktop_native/src/biometric/unix.rs
@@ -1,9 +1,26 @@
 use anyhow::{bail, Result};
+use pam_client::{Context, Flag};
+use pam_client::conv_cli::Conversation;
+use whoami;
 
 pub fn prompt(_hwnd: Vec<u8>, _message: String) -> Result<bool> {
-    bail!("platform not supported");
+    let mut context =Context::new(
+        "bitwarden", 
+        Some(&whoami::username()),
+        Conversation::new()
+    ).expect("Failed to initialize PAM context");
+
+    let result = context.authenticate(Flag::NONE);
+    if result.is_err() {
+        println!("Authentication failed");
+        println!("Error: {:?}", result.err().unwrap());
+        return Ok(false);
+    } else {
+        println!("Authentication succeeded");
+        return Ok(true);
+    }
 }
 
 pub fn available() -> Result<bool> {
-    bail!("platform not supported");
+    return Ok(true); //todo: check availability
 }
