a79f2f62dce7a4c5928488806d6401478c8995c8
Bernd Schoolmann <mail@quexten.com>
2023-01-28 02:14:55 +0100

    Change argon2 defaults

--- libs/common/src/enums/kdfType.ts
+++ libs/common/src/enums/kdfType.ts
@@ -5,9 +5,9 @@ export enum KdfType {
   Argon2id = 1,
 }
 
-export const DEFAULT_ARGON2_MEMORY = 19;
-export const DEFAULT_ARGON2_PARALLELISM = 1;
-export const DEFAULT_ARGON2_ITERATIONS = 2;
+export const DEFAULT_ARGON2_MEMORY = 64;
+export const DEFAULT_ARGON2_PARALLELISM = 4;
+export const DEFAULT_ARGON2_ITERATIONS = 3;
 
 export const DEFAULT_KDF_TYPE = KdfType.PBKDF2_SHA256;
 export const DEFAULT_PBKDF2_ITERATIONS = 600000;
