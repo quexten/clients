65772902447fcae95a9614b2fe081397f16bc4f0
Bernd Schoolmann <mail@quexten.com>
2023-08-02 00:15:30 +0200

    Add form validation to change-kdf component

--- apps/web/src/app/settings/change-kdf/change-kdf.component.ts
+++ apps/web/src/app/settings/change-kdf/change-kdf.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import { ChangeDetectorRef, Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 import { DialogServiceAbstraction } from "@bitwarden/angular/services/dialog";
 import { KdfConfig } from "@bitwarden/common/auth/models/domain/kdf-config";
@@ -9,6 +10,14 @@ import {
   DEFAULT_ARGON2_MEMORY,
   DEFAULT_ARGON2_PARALLELISM,
   KdfType,
+  MINIMUM_PBKDF2_ITERATIONS,
+  MAXIMUM_PBKDF2_ITERATIONS,
+  MINIMUM_ARGON2_ITERATIONS,
+  MAXIMUM_ARGON2_ITERATIONS,
+  MINIMUM_ARGON2_MEMORY,
+  MAXIMUM_ARGON2_MEMORY,
+  MINIMUM_ARGON2_PARALLELISM,
+  MAXIMUM_ARGON2_PARALLELISM,
 } from "@bitwarden/common/enums";
 import { StateService } from "@bitwarden/common/platform/abstractions/state.service";
 
@@ -25,37 +34,119 @@ export class ChangeKdfComponent implements OnInit {
   kdfOptions: any[] = [];
   recommendedPbkdf2Iterations = DEFAULT_PBKDF2_ITERATIONS;
 
-  constructor(private stateService: StateService, private dialogService: DialogServiceAbstraction) {
+  minimumPbkdf2Iterations = MINIMUM_PBKDF2_ITERATIONS;
+  maximumPbkdf2Iterations = MAXIMUM_PBKDF2_ITERATIONS;
+  minimumArgon2Iterations = MINIMUM_ARGON2_ITERATIONS;
+  maximumArgon2Iterations = MAXIMUM_ARGON2_ITERATIONS;
+  minimumArgon2Memory = MINIMUM_ARGON2_MEMORY;
+  maximumArgon2Memory = MAXIMUM_ARGON2_MEMORY;
+  minimumArgon2Parallelism = MINIMUM_ARGON2_PARALLELISM;
+  maximumArgon2Parallelism = MAXIMUM_ARGON2_PARALLELISM;
+
+  kdfForm: FormGroup;
+
+  constructor(
+    private stateService: StateService,
+    private dialogService: DialogServiceAbstraction,
+    private cd: ChangeDetectorRef
+  ) {
     this.kdfOptions = [
       { name: "PBKDF2 SHA-256", value: KdfType.PBKDF2_SHA256 },
       { name: "Argon2id", value: KdfType.Argon2id },
     ];
+
+    this.kdfForm = new FormGroup({
+      kdf: new FormControl(0, []),
+      iterations: new FormControl(DEFAULT_PBKDF2_ITERATIONS, []),
+      memory: new FormControl(0, []),
+      parallelism: new FormControl(0, []),
+    });
   }
 
   async ngOnInit() {
-    this.kdf = await this.stateService.getKdfType();
-    this.kdfConfig = await this.stateService.getKdfConfig();
-  }
+    const kdf = await this.stateService.getKdfType();
+    const kdfConfig = await this.stateService.getKdfConfig();
 
-  async onChangeKdf(newValue: KdfType) {
-    if (newValue === KdfType.PBKDF2_SHA256) {
-      this.kdfConfig = new KdfConfig(DEFAULT_PBKDF2_ITERATIONS);
-    } else if (newValue === KdfType.Argon2id) {
-      this.kdfConfig = new KdfConfig(
-        DEFAULT_ARGON2_ITERATIONS,
-        DEFAULT_ARGON2_MEMORY,
-        DEFAULT_ARGON2_PARALLELISM
-      );
+    if (kdf === KdfType.PBKDF2_SHA256) {
+      this.kdfForm.patchValue({
+        kdf: KdfType.PBKDF2_SHA256,
+        iterations: kdfConfig.iterations,
+      });
+    } else if (kdf === KdfType.Argon2id) {
+      this.kdfForm.patchValue({
+        kdf: KdfType.Argon2id,
+        iterations: kdfConfig.iterations,
+        memory: kdfConfig.memory,
+        parallelism: kdfConfig.parallelism,
+      });
     } else {
       throw new Error("Unknown KDF type.");
     }
+
+    this.cd.detectChanges();
+  }
+
+  async onChangeKdf() {
+    const newValue: KdfType = this.kdfForm.value.kdf;
+
+    if (newValue === KdfType.PBKDF2_SHA256) {
+      this.kdfForm.patchValue({
+        iterations: DEFAULT_PBKDF2_ITERATIONS,
+      });
+    } else if (newValue === KdfType.Argon2id) {
+      this.kdfForm.patchValue({
+        iterations: DEFAULT_ARGON2_ITERATIONS,
+        memory: DEFAULT_ARGON2_MEMORY,
+        parallelism: DEFAULT_ARGON2_PARALLELISM,
+      });
+    } else {
+      throw new Error("Unknown KDF type.");
+    }
+
+    await this.updateValidators(newValue);
+  }
+
+  async updateValidators(kdfType: KdfType) {
+    this.kdfForm.controls.iterations.clearValidators();
+    this.kdfForm.controls.memory.clearValidators();
+    this.kdfForm.controls.parallelism.clearValidators();
+
+    if (kdfType === KdfType.PBKDF2_SHA256) {
+      this.kdfForm.controls.iterations.setValidators([
+        Validators.min(this.minimumPbkdf2Iterations),
+        Validators.max(this.maximumPbkdf2Iterations),
+      ]);
+    } else if (kdfType === KdfType.Argon2id) {
+      this.kdfForm.controls.iterations.setValidators([
+        Validators.min(this.minimumArgon2Iterations),
+        Validators.max(this.maximumArgon2Iterations),
+      ]);
+      this.kdfForm.controls.memory.setValidators([
+        Validators.min(this.minimumArgon2Memory),
+        Validators.max(this.maximumArgon2Memory),
+      ]);
+      this.kdfForm.controls.parallelism.setValidators([
+        Validators.min(this.minimumArgon2Parallelism),
+        Validators.max(this.maximumArgon2Parallelism),
+      ]);
+    } else {
+      throw new Error("Unknown KDF type.");
+    }
+
+    this.kdfForm.controls.iterations.updateValueAndValidity();
+    this.kdfForm.controls.memory.updateValueAndValidity();
+    this.kdfForm.controls.parallelism.updateValueAndValidity();
   }
 
   async openConfirmationModal() {
     this.dialogService.open(ChangeKdfConfirmationComponent, {
       data: {
-        kdf: this.kdf,
-        kdfConfig: this.kdfConfig,
+        kdf: this.kdfForm.value.kdf,
+        kdfConfig: new KdfConfig(
+          this.kdfForm.value.iterations,
+          this.kdfForm.value.memory,
+          this.kdfForm.value.parallelism
+        ),
       },
     });
   }
--- libs/common/src/platform/services/crypto.service.ts
+++ libs/common/src/platform/services/crypto.service.ts
@@ -14,6 +14,11 @@ import {
   DEFAULT_ARGON2_MEMORY,
   DEFAULT_ARGON2_PARALLELISM,
   EncryptionType,
+  OLD_MINIMUM_PBKDF2_ITERATIONS,
+  MINIMUM_ARGON2_ITERATIONS,
+  MINIMUM_ARGON2_MEMORY,
+  MAXIMUM_ARGON2_MEMORY,
+  MINIMUM_ARGON2_PARALLELISM,
 } from "../../enums";
 import { Utils } from "../../platform/misc/utils";
 import { CryptoFunctionService } from "../abstractions/crypto-function.service";
@@ -419,30 +424,30 @@ export class CryptoService implements CryptoServiceAbstraction {
     let key: ArrayBuffer = null;
     if (kdf == null || kdf === KdfType.PBKDF2_SHA256) {
       if (kdfConfig.iterations == null) {
-        kdfConfig.iterations = 5000;
-      } else if (kdfConfig.iterations < 5000) {
-        throw new Error("PBKDF2 iteration minimum is 5000.");
+        kdfConfig.iterations = OLD_MINIMUM_PBKDF2_ITERATIONS;
+      } else if (kdfConfig.iterations < OLD_MINIMUM_PBKDF2_ITERATIONS) {
+        throw new Error(`PBKDF2 iteration minimum is ${OLD_MINIMUM_PBKDF2_ITERATIONS}.`);
       }
       key = await this.cryptoFunctionService.pbkdf2(password, salt, "sha256", kdfConfig.iterations);
     } else if (kdf == KdfType.Argon2id) {
       if (kdfConfig.iterations == null) {
         kdfConfig.iterations = DEFAULT_ARGON2_ITERATIONS;
-      } else if (kdfConfig.iterations < 2) {
-        throw new Error("Argon2 iteration minimum is 2.");
+      } else if (kdfConfig.iterations < MINIMUM_ARGON2_ITERATIONS) {
+        throw new Error(`Argon2 iteration minimum is ${MINIMUM_ARGON2_ITERATIONS}.`);
       }
 
       if (kdfConfig.memory == null) {
         kdfConfig.memory = DEFAULT_ARGON2_MEMORY;
-      } else if (kdfConfig.memory < 16) {
-        throw new Error("Argon2 memory minimum is 16 MB");
-      } else if (kdfConfig.memory > 1024) {
-        throw new Error("Argon2 memory maximum is 1024 MB");
+      } else if (kdfConfig.memory < MINIMUM_ARGON2_MEMORY) {
+        throw new Error(`Argon2 memory minimum is ${MINIMUM_ARGON2_MEMORY} MB`);
+      } else if (kdfConfig.memory > MAXIMUM_ARGON2_MEMORY) {
+        throw new Error(`Argon2 memory maximum is ${MAXIMUM_ARGON2_MEMORY} MB`);
       }
 
       if (kdfConfig.parallelism == null) {
         kdfConfig.parallelism = DEFAULT_ARGON2_PARALLELISM;
-      } else if (kdfConfig.parallelism < 1) {
-        throw new Error("Argon2 parallelism minimum is 1.");
+      } else if (kdfConfig.parallelism < MINIMUM_ARGON2_PARALLELISM) {
+        throw new Error(`Argon2 parallelism minimum is ${MINIMUM_ARGON2_PARALLELISM}.`);
       }
 
       const saltHash = await this.cryptoFunctionService.hash(salt, "sha256");
