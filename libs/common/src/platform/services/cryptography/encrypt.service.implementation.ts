cf470e3ad30c9890aeb7137ba39a47fa5bae3963
Bernd Schoolmann <mail@quexten.com>
2024-01-17 04:06:41 +0100

    Replace promise.all with for loop for performance reasons

--- libs/common/src/platform/services/cryptography/encrypt.service.implementation.ts
+++ libs/common/src/platform/services/cryptography/encrypt.service.implementation.ts
@@ -155,7 +155,12 @@ export class EncryptServiceImplementation implements EncryptService {
       return [];
     }
 
-    return await Promise.all(items.map((item) => item.decrypt(key)));
+    // don't use promise.all because this task is not io bound
+    let results = [];
+    for (let i = 0; i < items.length; i++) {
+      results.push(await items[i].decrypt(key));
+    }
+    return results;
   }
 
   private async aesEncrypt(data: Uint8Array, key: SymmetricCryptoKey): Promise<EncryptedObject> {
